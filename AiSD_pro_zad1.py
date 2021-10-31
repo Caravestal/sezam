87eb8713fcef663fa9a5b8e7402d2e30cc5160dd
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2021-10-31 22:23:10 +0100

    Update AiSD_pro_zad1.py

--- AiSD_pro_zad1.py
+++ AiSD_pro_zad1.py
@@ -8,6 +8,8 @@ class LinkedList:
     head: Node
     tail: Node
 
+# zad.1
+
     def __init__(self):
         self.head = None
         self.tail = None
@@ -19,20 +21,25 @@ class LinkedList:
         self.head = temp
 
     def append(self, value: Any) -> None:
-        temp = Node
-        temp.value = value
-        temp.next = self.tail
-        self.head = temp
+        temp = self.head
+        while (temp.next != None):
+            temp = temp.next
+        t = Node()
+        t.value = value
+        t.next = None
+        temp.next = t
 
     def node(self, at: int) -> Node:
-        n = self.head
+        temp = self.head
         for x in range(at):
-            n = n.next
-        return n
+            temp = temp.next
+        return temp
 
     def insert(self, value: Any, after: Node) -> None:
-        value.next = after.next
-        after.next = value
+        temp = Node
+        temp.value = value
+        temp.next = after.next
+        after.next = temp
 
     def pop(self) -> Any:
         temp = self.head
@@ -51,8 +58,15 @@ class LinkedList:
     def remove(self, after: Node) -> Any:
         after.next = after.next.next
 
-    def print(self):
-        print(str(self.head.value) + " -> " + str(self.head.next.value))
+    def __str__(self):
+        temp = self.head
+        wyn = ""
+        while(temp != None):
+            wyn += str(temp.value)
+            temp = temp.next
+            if(temp != None):
+                wyn += " -> "
+        return wyn
 
     def __len__(self):
         wyn = 0
@@ -61,3 +75,42 @@ class LinkedList:
             temp = temp.next
             wyn = wyn + 1
         return wyn
+
+list_ = LinkedList()
+
+assert list_.head == None
+
+list_.push(1)
+list_.push(0)
+
+assert str(list_) == '0 -> 1'
+
+list_.append(9)
+list_.append(10)
+
+assert str(list_) == '0 -> 1 -> 9 -> 10'
+
+middle_node = list_.node(at=1)
+list_.insert(5, after=middle_node)
+
+assert str(list_) == '0 -> 1 -> 5 -> 9 -> 10'
+
+first_element = list_.node(at=0)
+returned_first_element = list_.pop()
+
+assert first_element.value == returned_first_element
+
+last_element = list_.node(at=3)
+returned_last_element = list_.remove_last()
+
+assert last_element.value == returned_last_element
+assert str(list_) == '1 -> 5 -> 9'
+
+second_node = list_.node(at=1)
+list_.remove(second_node)
+
+assert str(list_) == '1 -> 5'
+
+print(len(list_))
+
+print(str(list_))
