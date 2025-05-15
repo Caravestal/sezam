d4c9e2eb101dcc589395f60f915e247574210a5f
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2025-05-15 16:06:50 +0200

    Update gpu_lab2.cu

--- gpu_lab2.cu
+++ gpu_lab2.cu
@@ -1,123 +1,275 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include "iostream"
+#include "cstdlib"
 #include <stdio.h>
 
-// zad 1
-__global__ void kernel() // kernel, to be executed on GPU
-{
-	// print the blocks and threads IDs
-	// warp = 32 threads. (128/32 = threads per block)
-	int warpIdValue = 0;
-	warpIdValue = threadIdx.x / 32;
+using namespace std;
 
-	printf(
-		"\nThe block ID is %d --- The thread ID is %d --- The Wardp ID %d",
-		blockIdx.x,
-		threadIdx.x,
-		warpIdValue
-	);
-}
-// zad2
-__global__ void kernel_dwa(int a)
+__global__ void z1_index_watku()
 {
-	int warpIdValue = 0;
-	warpIdValue = threadIdx.x / 32;
-	int wynik = warpIdValue * a;
-	printf("\n%d", wynik);
+	printf("%d\n",threadIdx.x);
 }
-// zad3
-__global__ void kernel_czy_podzielny_przez_trzy()
+
+__global__ void z2_pomnoz_a(int a)
+{
+	int warpIdx = 0;
+	warpIdx = threadIdx.x / 32;
+	int wynik = warpIdx * a;
+	printf("%d\n", wynik);
+}
+
+__global__ void z3_czy_podzielny_przez_trzy()
 {
 	if (threadIdx.x % 3 == 0)
 	{
-		printf("\nId watku %d jest podzielne przez 3", threadIdx.x);
-	}
-	else
-	{
-		printf("\nId watku %d nie jest podzielne przez 3", threadIdx.x);
+		printf("index watku: %d jest podzielny przez 3\n", threadIdx.x);
 	}
 }
-// zad4
-__global__ void kernel100_index_blok()
+
+__global__ void z4_100_suma()
 {
 	int a = 100;
-	int wynik = a + threadIdx.x + blockIdx.x;
-	printf("\n%d", wynik);
+	int wyn = a + threadIdx.x + blockIdx.x;
+	printf("wynik watku %d to %d\n", threadIdx.x, wyn);
 }
-// zad5
-__global__ void kernel_index_warp_parzyste()
-{
-	int warpIdValue = threadIdx.x / 32;
-	int a = threadIdx.x + blockIdx.x + warpIdValue;
-	if (a % 2 == 0)
-	{
-		printf("\n Suma identyfikatorow %d jest parzysta", a);
-	}
-	else
-	{
-		printf("\n Suma identyfikatorow %d jest nieparzysta", a);
-	}
-}
-// zad6
-__global__ void kernel_roznica_tab(int n, float* a, float* b, float* c)
-{
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (i < n)
-	{
-		c[i] = a[i] - b[i];
-	}
-}
-// zad7
-/*__global__ void parzyste_nieparzyste_tab(int n, float* a, float* b, float* c)
-{
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (i % 2 = 0)
-	{
-		c[i] = a[i] + b[i];
-	}
-	else
-	{
-		c[i] = a[i] - b[i];
-	}
-}*/
 
-int main() // function, execute on CPU
+__global__ void z5_czy_suma_indexow_parzysta()
 {
-	// format: <<<num_of_blocks, num_of_threads_per_block>>>
-	// kernel << <2, 128 >> > ();
-	// kernel_dwa << <2, 128 >> > (2);
-	// kernel_czy_podzielny_przez_trzy << <2, 128 >> > ();
-	// kernel100_index_blok << <2, 128 >> > ();
-	// kernel_index_warp_parzyste << <2, 128 >> > ();
+	int warpIdx = threadIdx.x / 32;
+	int allidx = threadIdx.x + warpIdx + blockIdx.x;
+	if (allidx % 2 == 0)
+	{
+		printf("%d suma indexu watku, bloku i warpa to %d i jest parzysta\n", threadIdx.x, allidx);
+	}
+}
+
+#define N 1024
+#define BLOCK_SIZE 256
+
+__global__ void z6_roznica(float* a, float* b, float* c)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < N)
+	{
+		c[idx] = a[idx] - b[idx];
+	}
+}
+
+void z6()
+{
+	float* cpu_a = new float[N];
+	float* cpu_b = new float[N];
+	float* cpu_c = new float[N];
+	float* gpu_a, * gpu_b, * gpu_c;
+
+	for (int i = 0;i < N;i++)
+	{
+		cpu_a[i] = static_cast<float>(i+10);
+		cpu_b[i] = static_cast<float>(i);
+	}
+
+	cudaMalloc(&gpu_a, N * sizeof(float));
+	cudaMalloc(&gpu_b, N * sizeof(float));
+	cudaMalloc(&gpu_c, N * sizeof(float));
+
+	cudaMemcpy(gpu_a, cpu_a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_b, cpu_b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+	int gridsize = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+	z6_roznica <<< gridsize, BLOCK_SIZE >>> (gpu_a, gpu_b, gpu_c);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(cpu_c, gpu_c, N * sizeof(float), cudaMemcpyDeviceToHost);
 	
-	int n = 1000;
-	int * a, * b, * c; // wskaźniki do tablic
-	int * d_a, * d_b, * d_c; // wskaźniki na pamięć GPU
-	a = new int[n];
-	b = new int[n]; // alokacja pamięci do RAM
-	c = new int[n];
-	cudaMalloc(&d_a, n * sizeof(int));
-	cudaMalloc(&d_b, n * sizeof(int)); // alokacja na GPU
-	cudaMalloc(&d_c, n * sizeof(int));
-	cudaMemcpy(d_a, a, n * sizeof(int));
-	cudaMemcpy(d_b, a, n * sizeof(int)); // kopiowanie danych do GPU
-	cudaMemcpyHostToDevice;
-	int blockSize = 256;
-	int numBlocks = (n + blockSize - 1) / blockSize; //konfiguracja parametrów kernela
+	for (int i = 0;i < N;i++)
+	{
+		printf("%f\n", cpu_c[i]);
+	}
 
-	kernel_roznica_tab << blockDim, blockSize >> > ();
-
-
-	return 0;
+	delete[]cpu_a;
+	delete[]cpu_b;
+	delete[]cpu_c;
+	cudaFree(gpu_a);
+	cudaFree(gpu_b);
+	cudaFree(gpu_c);
 }
-int main() // function, execute on CPU
+
+__global__ void z7_suma_roznica(float* a, float* b, float* c)
 {
-	// format: <<<num_of_blocks, num_of_threads_per_block>>>
-	// kernel << <2, 128 >> > ();
-	// kernel_dwa << <2, 128 >> > (2);
-	// kernel_czy_podzielny_przez_trzy << <2, 128 >> > ();
-	// kernel100_index_blok << <2, 128 >> > ();
-	kernel_index_warp_parzyste << <2, 128 >> > ();
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < N)
+	{
+		if (idx % 2 == 0)
+			c[idx] = a[idx] + b[idx];
+		else
+			c[idx] = a[idx] - b[idx];
+	}
+}
+
+void z7()
+{
+	float* cpu_a = new float[N];
+	float* cpu_b = new float[N];
+	float* cpu_c = new float[N];
+	float* gpu_a, * gpu_b, * gpu_c;
+
+	for (int i = 0;i < N;i++)
+	{
+		cpu_a[i] = static_cast<float>(i + 10);
+		cpu_b[i] = static_cast<float>(i);
+	}
+
+	cudaMalloc(&gpu_a, N * sizeof(float));
+	cudaMalloc(&gpu_b, N * sizeof(float));
+	cudaMalloc(&gpu_c, N * sizeof(float));
+
+	cudaMemcpy(gpu_a, cpu_a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_b, cpu_b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+	int gridsize = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+	z7_suma_roznica << <gridsize, BLOCK_SIZE >> > (gpu_a, gpu_b, gpu_c);
+
+	cudaMemcpy(cpu_c, gpu_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+	for (int i = 0;i < N;i++)
+	{
+		printf("%f\n", cpu_c[i]);
+	}
+
+	free(cpu_a);
+	free(cpu_b);
+	free(cpu_c);
+	cudaFree(gpu_a);
+	cudaFree(gpu_b);
+	cudaFree(gpu_c);
+}
+
+__global__ void z8_czy_warp_parzyste(float* a, float* b, float* c)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int warpIdx = threadIdx.x / 32;
+	if (idx < N)
+	{
+		if (warpIdx % 2 == 0)
+			c[idx] = a[idx] + b[idx];
+		else
+			c[idx] = a[idx] - b[idx];
+	}
+}
+
+void z8()
+{
+	float* cpu_a = new float[N];
+	float* cpu_b = new float[N];
+	float* cpu_c = new float[N];
+	float* gpu_a, * gpu_b, * gpu_c;
+
+	for (int i = 0;i < N;i++)
+	{
+		cpu_a[i] = static_cast<float>(i + 10);
+		cpu_b[i] = static_cast<float>(i);
+	}
+
+	cudaMalloc(&gpu_a, N * sizeof(float));
+	cudaMalloc(&gpu_b, N * sizeof(float));
+	cudaMalloc(&gpu_c, N * sizeof(float));
+
+	cudaMemcpy(gpu_a, cpu_a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_b, cpu_b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+	int gridsize = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+	z8_czy_warp_parzyste << <gridsize, BLOCK_SIZE >> > (gpu_a, gpu_b, gpu_c);
+
+	cudaMemcpy(cpu_c, gpu_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+	for (int i = 0;i < N;i++)
+	{
+		printf("%f\n", cpu_c[i]);
+	}
+
+	free(cpu_a);
+	free(cpu_b);
+	free(cpu_c);
+	cudaFree(gpu_a);
+	cudaFree(gpu_b);
+	cudaFree(gpu_c);
+}
+
+__global__ void z9_suma_duzych_wektorow(float* a, float* b, float* c)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < N)
+	{
+		c[idx] = a[idx] + b[idx];
+	}
+}
+
+void z9()
+{
+	int super_duper_duze = 16 * 1024 * 1024 * 1024/ sizeof(float); //16GB, na potrzeby sprawdzenie usuń dwa 1024
+	float* a = new float[super_duper_duze];
+	float* b = new float[super_duper_duze];
+	float* af = new float[N];
+	float* bf = new float[N];
+	float* c = new float[N];
+	float* da, * db, * dc;
+
+	for (int i = 0; i < super_duper_duze; i++)
+	{
+		a[i] = static_cast<float>(i);
+		b[i] = static_cast<float>(i);
+	}
+
+	cudaMalloc(&da, N * sizeof(float));
+	cudaMalloc(&db, N * sizeof(float));
+	cudaMalloc(&dc, N * sizeof(float));
+
+	int gridsize = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+	for (int i = 0; i < super_duper_duze / N;i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			af[j] = a[i * N + j];
+			bf[j] = b[i * N + j];
+		}
+		cudaMemcpy(da, af, N * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(db, bf, N * sizeof(float), cudaMemcpyHostToDevice);
+
+		z9_suma_duzych_wektorow << < gridsize, BLOCK_SIZE >> > (da, db, dc);
+
+
+		cudaMemcpy(c, dc, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+		for (int z = 0;z < N;z++)
+		{
+			printf("%f\n", c[z]);
+		}
+	}
+	free(a);
+	free(af);
+	free(b);
+	free(bf);
+	free(c);
+	cudaFree(da);
+	cudaFree(db);
+	cudaFree(dc);
+}
+
+int main()
+{
+	//z1_index_watku << <2, 128 >> > ();
+	//z2_pomnoz_a << <2, 128 >> > (2);
+	//z3_czy_podzielny_przez_trzy << <2, 128 >> > ();
+	//z4_100_suma << <2, 128 >> > ();
+	//z5_czy_suma_indexow_parzysta << <2, 128 >> > ();
+	//z6();
+	//z7();
+	//z8();
+	//z9();
 	return 0;
 }
